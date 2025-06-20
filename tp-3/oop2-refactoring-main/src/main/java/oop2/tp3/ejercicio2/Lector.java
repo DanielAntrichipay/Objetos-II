b14bfa756a267bf29a29002c81a7d8257ff7a7d1
DanielAntrichipay <danielantrichipay20@gmail.com>
2025-06-20 13:36:57 -0300

    CORRECCION - Punto dos finalizado

--- tp-3/oop2-refactoring-main/src/main/java/oop2/tp3/ejercicio2/Lector.java
+++ tp-3/oop2-refactoring-main/src/main/java/oop2/tp3/ejercicio2/Lector.java
@@ -1,4 +1,36 @@
 package oop2.tp3.ejercicio2;
 
+import com.opencsv.CSVReader;
+
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Lector {
+    private String path;
+
+    public Lector(String path) {
+        this.path = path;
+    }
+
+    public List<String[]> recuperarFila (){
+        try {
+            List<String[]> csvData = new ArrayList<String[]>();
+            CSVReader reader = null;
+            reader = new CSVReader(new FileReader(this.path /*"src/main/resources/data.csv"*/));
+
+            String[] row = null;
+
+            while ((row = reader.readNext()) != null) {
+                csvData.add(row);
+            }
+
+            reader.close();
+            csvData.remove(0);
+            return csvData;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
--- tp-3/oop2-refactoring-main/src/main/java/oop2/tp3/ejercicio2/Recaudacion.java
+++ tp-3/oop2-refactoring-main/src/main/java/oop2/tp3/ejercicio2/Recaudacion.java
@@ -1,8 +1,5 @@
 package oop2.tp3.ejercicio2;
 
-import com.opencsv.CSVReader;
-
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -10,79 +7,59 @@ import java.util.List;
 import java.util.Map;
 
 public class Recaudacion {
-    public static List<Map<String, String>> where(Map<String, String> options)
-            throws IOException {
-        List<String[]> csvData = new ArrayList<String[]>();
-        CSVReader reader = new CSVReader(new FileReader("src/main/resources/data.csv"));
-        String[] row = null;
+    public static final String COMPANY_NAME = "company_name";
+    public static final String CITY = "city";
+    public static final String STATE = "state";
+    public static final String ROUND = "round";
+    public static final int NUMERO_DE_COLUMNA_COMPANY_NAME = 1;
+    public static final int NUMERO_DE_COLUMNA_CITY = 4;
+    public static final int NUMERO_DE_COLUMNA_STATE = 5;
+    public static final int NUMERO_DE_COLUMNA_ROUN = 9;
+    private List<String[]> datos;
 
-        while ((row = reader.readNext()) != null) {
-            csvData.add(row);
-        }
+    public Recaudacion(Lector lectorDeCSVs) {
+        this.datos = lectorDeCSVs.recuperarFila();
+    }
 
-        reader.close();
-        csvData.remove(0);
+    public List<Map<String, String>> where(Map<String, String> options) throws IOException {
+        filtrarPorColumna(options, COMPANY_NAME, NUMERO_DE_COLUMNA_COMPANY_NAME);
+        filtrarPorColumna(options, CITY, NUMERO_DE_COLUMNA_CITY);
+        filtrarPorColumna(options, STATE, NUMERO_DE_COLUMNA_STATE);
+        filtrarPorColumna(options, ROUND, NUMERO_DE_COLUMNA_ROUN);
 
-        if (options.containsKey("company_name")) {
-            List<String[]> results = new ArrayList<String[]>();
+        return mapear();
+    }
 
-            for (int i = 0; i < csvData.size(); i++) {
-                if (csvData.get(i)[1].equals(options.get("company_name"))) {
-                    results.add(csvData.get(i));
-                }
-            }
-            csvData = results;
-        }
-
-        if (options.containsKey("city")) {
-            List<String[]> results = new ArrayList<String[]>();
-
-            for (int i = 0; i < csvData.size(); i++) {
-                if (csvData.get(i)[4].equals(options.get("city"))) {
-                    results.add(csvData.get(i));
-                }
-            }
-            csvData = results;
-        }
-
-        if (options.containsKey("state")) {
-            List<String[]> results = new ArrayList<String[]>();
-
-            for (int i = 0; i < csvData.size(); i++) {
-                if (csvData.get(i)[5].equals(options.get("state"))) {
-                    results.add(csvData.get(i));
-                }
-            }
-            csvData = results;
-        }
-
-        if (options.containsKey("round")) {
-            List<String[]> results = new ArrayList<String[]>();
-
-            for (int i = 0; i < csvData.size(); i++) {
-                if (csvData.get(i)[9].equals(options.get("round"))) {
-                    results.add(csvData.get(i));
-                }
-            }
-            csvData = results;
-        }
-
+    private List<Map<String, String>> mapear() {
         List<Map<String, String>> output = new ArrayList<Map<String, String>>();
 
-        for (int i = 0; i < csvData.size(); i++) {
+        for (int i = 0; i < this.datos.size(); i++) {
             Map<String, String> mapped = new HashMap<String, String>();
-            mapped.put("permalink", csvData.get(i)[0]);
-            mapped.put("company_name", csvData.get(i)[1]);
-            mapped.put("number_employees", csvData.get(i)[2]);
-            mapped.put("category", csvData.get(i)[3]);
-            mapped.put("city", csvData.get(i)[4]);
-            mapped.put("state", csvData.get(i)[5]);
-            mapped.put("funded_date", csvData.get(i)[6]);
-            mapped.put("raised_amount", csvData.get(i)[7]);
-            mapped.put("raised_currency", csvData.get(i)[8]);
-            mapped.put("round", csvData.get(i)[9]);
+            mapped.put("permalink", this.datos.get(i)[0]);
+            mapped.put(COMPANY_NAME, this.datos.get(i)[NUMERO_DE_COLUMNA_COMPANY_NAME]);
+            mapped.put("number_employees", this.datos.get(i)[2]);
+            mapped.put("category", this.datos.get(i)[3]);
+            mapped.put(CITY, this.datos.get(i)[NUMERO_DE_COLUMNA_CITY]);
+            mapped.put(STATE, this.datos.get(i)[NUMERO_DE_COLUMNA_STATE]);
+            mapped.put("funded_date", this.datos.get(i)[6]);
+            mapped.put("raised_amount", this.datos.get(i)[7]);
+            mapped.put("raised_currency", this.datos.get(i)[8]);
+            mapped.put(ROUND, this.datos.get(i)[NUMERO_DE_COLUMNA_ROUN]);
             output.add(mapped);
         }
         return output;
+    }
+
+    private void filtrarPorColumna(Map<String, String> options, String nombreColumna, int numeroDeColumna) {
+        if (options.containsKey(nombreColumna)) {
+            List<String[]> results = new ArrayList<String[]>();
+
+            for (String[] dato : this.datos) {
+                if (dato[numeroDeColumna].equals(options.get(nombreColumna))) {
+                    results.add(dato);
+                }
+            }
+            this.datos = results;
+        }
     }
 }
